5bad9912916625f3373fb0fce146a2f2af1f9ec2
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-10 11:01:52 -0300

    nested route was added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :songs
 
   resources :playlists, only: :show do
-    resources :songs, only: [:new, :create]
+    resources :songs, only: [:new, :create, :show]
   end
+  # get 'playlists/:playlist_id/songs/:id', to: 'songs#show', as: 'playlist_song'
+  #same as showed at rails routes
 
   # above same as: /playlists/:playlist_id/songs/new(.:format)
   # creating a new song for this playlist
