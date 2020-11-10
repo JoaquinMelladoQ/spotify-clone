1fa44af9fad84f66d9c6b1ffa7fbf0feb7bb4447
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-09 23:09:01 -0300

    added nested root to create a song within a playlist

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :playlists
   resources :songs
 
+  resources :playlists, only: :show do
+    resources :songs, only: [:new, :create]
+  end
+
+  # above same as: /playlists/:playlist_id/songs/new(.:format)
+  # creating a new song for this playlist
+
   root 'playlists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
